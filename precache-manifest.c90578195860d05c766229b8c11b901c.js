d5b4d7f9529da1e1d227a0889286f21339ab6600
GorWEB <brutyan.99@mail.ru>
2020-05-28 18:41:32 +0000

    Updates

---
+++ precache-manifest.c90578195860d05c766229b8c11b901c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64d60b01ba485ffec49f542e89b4411",
+    "url": "/registration-form/index.html"
+  },
+  {
+    "revision": "f034e6240923c5088ed8",
+    "url": "/registration-form/static/css/main.b1658f52.chunk.css"
+  },
+  {
+    "revision": "d4351b4acd67d8791149",
+    "url": "/registration-form/static/js/2.a370f923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration-form/static/js/2.a370f923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f034e6240923c5088ed8",
+    "url": "/registration-form/static/js/main.8a62c08b.chunk.js"
+  },
+  {
+    "revision": "cd33ebde668fe4cc2445",
+    "url": "/registration-form/static/js/runtime-main.5a0efc0c.js"
+  }
+]);
